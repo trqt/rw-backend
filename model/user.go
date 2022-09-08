7faff234c4bb250b1a907a3068b4feb41e9bdd93
Guilherme Torquato <30443527+trqt@users.noreply.github.com>
2022-09-08 10:32:10 -0300

    Updated User Model

--- model/user.go
+++ model/user.go
@@ -9,12 +9,14 @@ import (
 type User struct {
 	gorm.Model
 	ID          uint           `json:"id"`
-	Role        string         `gorm:"not null" json:"type"`
+	Role        string         `gorm:"not null" json:"role"`
 	Cpf         string         `gorm:"not null,unique" json:"cpf"`
 	Name        string         `gorm:"not null,type:text" json:"name"`
 	Password    []byte         `gorm:"not null" json:"password"`
 	Email       string         `gorm:"not null,unique" json:"email"`
 	Description string         `gorm:"type:text" json:"description"`
+	Tags        string         `gorm:"not null,type:text" json:"tags"`
+	Photo       string         `gorm:"not null" json:"photo_url"`
 	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index,->" json:"-"`
